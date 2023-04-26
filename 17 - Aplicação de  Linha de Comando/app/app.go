c38a5fd318460e2469b11c34d2061ab1f3663159
Lyarkh <lcemanuel.emanuel@gmail.com>
2023-04-26 09:43:32 -0300

    feat: adicionando nova funcionalidade de servidores
    funcionalidade onde se retorna os servidores onde está hospedado o domínio informado

--- 17 - Aplicação de  Linha de Comando/app/app.go
+++ 17 - Aplicação de  Linha de Comando/app/app.go
@@ -28,6 +28,12 @@ func Gerar() *cli.App {
 			Flags:  flags,
 			Action: buscarIps,
 		},
+		{
+			Name:   "servidores",
+			Usage:  "Busca o nome dos servidores na internet",
+			Flags:  flags,
+			Action: buscarServidores,
+		},
 	}
 	return app
 }
@@ -42,5 +48,18 @@ func buscarIps(c *cli.Context) {
 
 	for _, ip := range ips {
 		fmt.Println(ip)
+	}
+}
+
+func buscarServidores(c *cli.Context) {
+	host := c.String("host")
+
+	servidores, erro := net.LookupNS(host)
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	for _, servidor := range servidores {
+		fmt.Println(servidor.Host)
 	}
 }
